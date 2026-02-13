48b0c5c788621e9746d2d3238e57692905b2b548
Daniel Lerche <daniel.lerche10@gmail.com>
2026-02-13 12:52:47 +0100

    feat(jpa): Added User entity

---
+++ backend/src/main/java/backend/entity/User.java
@@ -0,0 +1,52 @@
+package backend.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String email;
+    private Long imageId;
+    private String role;
+    private String pincode;
+
+    @ManyToOne
+    @JoinColumn(name = "Family_Email", referencedColumnName = "Email")
+    private Family family;
+
+    @ManyToOne 
+    @JoinColumn(name = "Image_ID")
+    private Image image;
+
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Message> messages;
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_task",
+        joinColumns = @JoinColumn(name = "User_ID"),
+        inverseJoinColumns = @JoinColumn(name = "Task_ID")
+    )
+    private List<Task> tasks;
+}
