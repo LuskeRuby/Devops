f228e41ba257bd09a347ddb3eca59c9a1df8e539
Daniel Lerche <daniel.lerche10@gmail.com>
2026-02-13 13:04:37 +0100

    feat(jpa): Added Family entity

---
+++ backend/src/main/java/backend/entity/Family.java
@@ -0,0 +1,27 @@
+package backend.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter 
+@Setter 
+@NoArgsConstructor
+public class Family {
+
+    @Id
+    @Column(name = "Email")
+    private String email;
+
+    private String password;
+
+    @OneToMany(mappedBy = "family")
+    private List<User> users;
+}
