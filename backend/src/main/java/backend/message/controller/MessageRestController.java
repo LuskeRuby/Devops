116c35dc096fe274faebe3ec7a8e0b096c0114ac
Rasmus <s245648@student.dtu.dk>
2026-02-21 15:50:17 +0100

    websocket now works with db aswell

--- backend/src/main/java/backend/message/controller/MessageRestController.java
+++ backend/src/main/java/backend/message/controller/MessageRestController.java
@@ -1,11 +1,4 @@
 package backend.message.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/messages")
 public class MessageRestController {
-
-
 }
