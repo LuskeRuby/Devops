8b551f7aa2eb70aaa6cbb89f9a13609576d07284
Rasmus <s245648@student.dtu.dk>
2026-02-21 09:58:43 +0100

    Restore Message entity and repository (accidentally deleted)

---
+++ backend/src/main/java/backend/message/Message.java
@@ -0,0 +1,32 @@
+package backend.message;
+
+import java.time.LocalDateTime;
+
+import backend.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter 
+@Setter 
+@NoArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "User_ID")
+    private User user;
+}
