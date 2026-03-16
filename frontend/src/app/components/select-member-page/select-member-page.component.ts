3fba8f2efe1423cea472fdf4a4e426e92ba76f9b
Viktor <viktorthysen@gmail.com>
2026-03-16 10:12:22 +0100

    added the rewards system to the select-member-page.component.ts and also removed the ability to create a member unless there werent any members in the family

--- frontend/src/app/components/select-member-page/select-member-page.component.ts
+++ frontend/src/app/components/select-member-page/select-member-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { UserService, User } from '../../services/user.service';
@@ -50,4 +50,11 @@ export class SelectMemberPageComponent implements OnInit {
   addMember(): void {
     this.router.navigate(['/create-member-page']);
   }
+
+  calculateRewards(totalPoints: number): number {
+    const targetPoints = 100;
+    return Math.floor(totalPoints / targetPoints);
+  }
 }
+
+
