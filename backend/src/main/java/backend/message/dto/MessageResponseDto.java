5057dc3751a4aac967f4ea4a2697b0060b8e5f0a
Rasmus <s245648@student.dtu.dk>
2026-03-23 18:45:22 +0100

    images

--- backend/src/main/java/backend/message/dto/MessageResponseDto.java
+++ backend/src/main/java/backend/message/dto/MessageResponseDto.java
@@ -8,7 +8,8 @@ public record MessageResponseDto(
         String content,
         LocalDateTime timestamp,
         String username,
-        Long userId
+        Long userId,
+        Long imageId
 ) {
     public MessageResponseDto(Message message) {
         this(
@@ -16,7 +17,8 @@ public record MessageResponseDto(
                 message.getContent(),
                 message.getTimestamp(),
                 message.getUser().getName(),
-                message.getUser().getId()
+                message.getUser().getId(),
+                message.getUser().getImage() != null ? message.getUser().getImage().getId() : null
         );
     }
 }
